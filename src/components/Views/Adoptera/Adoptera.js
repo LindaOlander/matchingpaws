f9d08808d07c7b4e2eb5af3f8121aabb4e9b0031
Linda Olander <lindaolander@users.noreply.github.com>
2021-02-02 18:06:54 +0100

    Manifest and new wizard hero

--- src/components/Views/Adoptera/Adoptera.js
+++ src/components/Views/Adoptera/Adoptera.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Navbar from '../../Navbar/Navbar';
+import NavbarBlack from '../../Navbar/NavbarBlack';
 import WizardParent from '../../StepWizard/Wizard/WizardParent';
 
 const Adoptera = () => {
     return (
         <>
-            <Navbar color="white" />
+            <NavbarBlack color="black" />
             <div className="headerWizard">
                 <h2>Hitta din match</h2>
                 <p>Låt vårt matchningsverktyg identifiera lämpliga matchningar till dig.</p>
