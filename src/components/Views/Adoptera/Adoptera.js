a06dae484d680607a1dfccd2a0b889f928841d6e
Linda Olander <lindaolander@users.noreply.github.com>
2021-02-02 13:39:45 +0100

    New image for wizard

--- src/components/Views/Adoptera/Adoptera.js
+++ src/components/Views/Adoptera/Adoptera.js
@@ -9,17 +9,14 @@ import WizardParent from '../../StepWizard/Wizard/WizardParent';
 const Adoptera = () => {
     return (
         <>
-            <NavbarBlack color="black" />
+            <NavbarBlack color="white" />
             <div className="headerWizard">
                 <h2>Hitta din match</h2>
                 <p>Låt vårt matchningsverktyg identifiera lämpliga matchningar till dig.</p>
-                <p>Var noga med att svara sanningsenligt på testet - för dig och din framtida hunds skull!</p>
+                <p>Var noga med att svara sanningsenligt på testet, <br />för dig och din framtida hunds skull!</p>
                 <a href="#quiz"><button>Starta testet</button></a>
             </div>
             <WizardParent />
-            {/* <Wizard /> */}
-            {/* <Quiz /> */}
-            {/* <DogList /> */}
         </>
     )
 }
